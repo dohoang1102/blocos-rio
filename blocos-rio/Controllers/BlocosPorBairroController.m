1cfe8890ee1c867d346e36e81ee8610ec3316b2c
Felipe Cypriano <fmcypriano@gmail.com>
2011-02-25 02:30:16 -0300

    Lista de bairros com gradient como background

--- blocos-rio/Controllers/BlocosPorBairroController.m
+++ blocos-rio/Controllers/BlocosPorBairroController.m
@@ -126,6 +126,15 @@
     if (cell == nil) {
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId] autorelease];
 		cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+        UIView *gradientView = [[[UIView alloc] initWithFrame:cell.frame] autorelease];
+        CAGradientLayer *gradient = [CAGradientLayer layer];
+        gradient.frame = gradientView.bounds;
+        gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor whiteColor] CGColor], (id)[[UIColor colorWithRed:0.866 green:0.866 blue:0.866 alpha:1] CGColor], nil];
+        [gradientView.layer insertSublayer:gradient atIndex:0];
+        cell.backgroundView = gradientView;
+        cell.contentView.backgroundColor = [UIColor clearColor];
+        cell.textLabel.backgroundColor = [UIColor clearColor];
     }
     
     cell.textLabel.text = [bairro valueForKey:@"nome"];
